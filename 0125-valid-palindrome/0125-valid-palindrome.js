2120aac1de801fe9338805457d48b00537c8bbdd
Kristina <113807480+kristina-lim@users.noreply.github.com>
2023-01-06 12:42:27 -0800

    Time: 83 ms (76.61%), Space: 46.4 MB (47.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -6,4 +6,6 @@ var isPalindrome = function(s) {
     s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
     return s === s.split('').reverse().join('');
     // return s.split('').reverse().join('') === s.toLowerCase().replace(/ /g, '');
+
+
 };
