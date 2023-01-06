12abf07c33d70e229d8e093b9e4b20d094ee42cf
Kristina <113807480+kristina-lim@users.noreply.github.com>
2023-01-05 16:02:06 -0800

    Time: 182 ms (19.27%), Space: 44.4 MB (81.19%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0;
+    while (i < nums.length) {
+        if (nums[i] === nums[i + 1]) {
+            nums.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+};
