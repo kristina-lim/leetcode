b045a30bfc533e7cbbaf22b4e07b4a0dbf06e4ce
Kristina <113807480+kristina-lim@users.noreply.github.com>
2023-01-06 13:38:00 -0800

    Time: 87 ms (70.91%), Space: 47.3 MB (34.81%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,9 +3,8 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    return s === s.split('').reverse().join('');
-    // return s.split('').reverse().join('') === s.toLowerCase().replace(/ /g, '');
-
-
+    // /[^a-z0-9]/g: ^ symbol indicates negation, so whatever is followed after that it (a-z0-9) won't match any character thats in this set.
+    // s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // return s === s.split('').reverse().join('');
+    return s.toLowerCase().replace(/[^a-z0-9]/g, '').split('').reverse().join('') === s.toLowerCase().replace(/[^a-z0-9]/g, '');
 };
