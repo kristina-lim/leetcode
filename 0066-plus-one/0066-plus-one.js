f934c89c09282deb6ba11920b1cffa3b686f9144
Kristina <113807480+kristina-lim@users.noreply.github.com>
2023-01-05 17:57:55 -0800

    Time: 87 ms (49.75%), Space: 42.2 MB (28.45%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let total = 1n;
+    number = BigInt(digits.join(''));
+    total += number;
+    let result = total.toString().split('');
+    return result;
+    // not right... but good job for trying kris
+    // return (BigInt(digits.join('') + 1n)).toString().split('');
+};
