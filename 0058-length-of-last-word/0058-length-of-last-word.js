24dc64d0d1da9951cf1881cae6c947efa306bf64
Kristina <113807480+kristina-lim@users.noreply.github.com>
2023-02-06 17:01:45 -0800

    Time: 46 ms (99.72%), Space: 41.5 MB (92.29%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let trimmed = s.trim();
-    return trimmed.length - trimmed.lastIndexOf(' ') - 1;
+    // let trimmed = s.trim();
+    // return trimmed.length - trimmed.lastIndexOf(' ') - 1;
+    return s.trim().split(' ').pop().length;
 };
