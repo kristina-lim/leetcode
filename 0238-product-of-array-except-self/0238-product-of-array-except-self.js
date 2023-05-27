015e59a70cb065dbe2899d6faed8d1c88b8aab58
Kristina <113807480+kristina-lim@users.noreply.github.com>
2023-05-26 22:43:30 -0700

    Time: 91 ms (94.97%), Space: 54.8 MB (44.18%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let result = [];
+    let left = 1;
+    let right = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    // compute left product for nums[i]
+    result[i] = left;
+    left *= nums[i];
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    // compute right product for nums[i] and multiply it with the left product
+    result[i] *= right;
+    right *= nums[i];
+  }
+
+  return result;
+};
