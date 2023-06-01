fda2add69e0540aec658e2093f5dba9225da9786
Kristina <113807480+kristina-lim@users.noreply.github.com>
2023-05-31 20:54:38 -0700

    Time: 66 ms (93.16%), Space: 44.6 MB (88.51%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -9,34 +9,24 @@ var isValidSudoku = function(board) {
         box = new Set();
 
     for (let j = 0; j < 9; j++) {
-      let currentRow = board[i][j];
-      let currentCol = board[j][i];
-      let checkBox = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
+      let _row = board[i][j];
+      let _col = board[j][i];
+      let _box = board[3*Math.floor(i/3)+Math.floor(j/3)][3*(i%3)+(j%3)]
       
-      if (currentRow != '.') {
-        if (row.has(currentRow)) return false;
-        row.add(currentRow);
+      if (_row != '.') {
+        if (row.has(_row)) return false;
+        row.add(_row);
       }
-      if (currentCol != '.') {
-        if (col.has(currentCol)) return false;
-        col.add(currentCol);
+      if (_col != '.') {
+        if (col.has(_col)) return false;
+        col.add(_col);
       }
       
-      if (checkBox != '.') {
-        if (box.has(checkBox)) return false;
-        box.add(checkBox);
+      if (_box != '.') {
+        if (box.has(_box)) return false;
+        box.add(_box);
       } 
     }
   }
   return true
 };
-
-//A Sudoku board is a 9x9 grid, divided into 9 smaller 3x3 grids, and the goal is to fill in the numbers 1 to 9 in such a way that each row, column, and smaller grid contains each number exactly once.
-
-//The code goes through each row, column, and smaller grid in the board, and for each of them, it creates a new Set. A Set is like a special kind of list that only allows unique values - you can't add the same value twice.
-
-//Then, it goes through each cell in that row, column, or smaller grid, and checks if it contains a number or a dot. If it contains a number, it checks if that number is already in the Set. If it is, that means there are two of the same number in that row, column, or smaller grid, which is not allowed in a valid Sudoku. So the function returns false.
-
-//If the number is not in the Set yet, it adds it to the Set.
-
-//After checking all the cells in each row, column, and smaller grid, the function returns true if no duplicates were found. Otherwise, it returns false.
